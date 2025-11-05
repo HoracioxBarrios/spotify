c86c94a2a6b07149b8800671a2d3ac2151af4df7
HoracioxBarrios <horacioxbarrios@gmail.com>
2025-11-05 19:36:59 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
+  declarations: [//TODO: aca se puede DECLARAR directivas, pipes, componentes
     AppComponent
   ],
-  imports: [
+  imports: [//TODO: aca SOLO SE IMPORTAN otros modulos
     BrowserModule,
     AppRoutingModule
   ],
