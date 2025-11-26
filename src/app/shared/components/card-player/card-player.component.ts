dec2ce72662a092b2ca5fc56fb84d781c8d31c41
HoracioxBarrios <horacioxbarrios@gmail.com>
2025-11-25 21:18:02 -0300

    avance con la implementacion de interface e inputs en seccion generica , y el componente hijo que se visualiza card-player

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TracksModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-card-player',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-player.component.css']
 })
 export class CardPlayerComponent implements OnInit {
+@Input() mode :"small" | "big" = "small";
+@Input() track!: TracksModel;
+
+//Otra forma opcional es esta, inicializando algo asi: 
+// @Input() track : TracksModel = {
+//   id: 0,
+//   name: "",
+//   album: "",
+//   cover: "",
+//   artist: {name: "", nickName: "", nacionality: ""},
+//   duration: {start: 0, end: 0},
+//   url: ""
+// };
 
   constructor() { }
 
